5c931db840c0b978435c7310d58ca63e8e2d4ef3
William <wcooper32@gatech.edu>
2020-09-21 01:00:16 -0400

    Begin Implementation of Initial Farm UI

--- src/main/screens/GameScreen.java
+++ src/main/screens/GameScreen.java
@@ -1,7 +1,14 @@
 package main.screens;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import main.engine.Manager;
+
+import java.util.Stack;
+
 
 /**
  * 1. Have different class/app for UI (money, date, etc.)
@@ -16,11 +23,36 @@ public class GameScreen {
     public GameScreen(Manager manager) {
         root = new StackPane();
 
-        // Farm plots
-
         // Game Overlay
         OverlayScreen overlayScreen = new OverlayScreen(manager);
         root.getChildren().add(overlayScreen.root);
+
+        // Making plots inside a grid
+        GridPane gridPane = new GridPane();
+        root.getChildren().add(gridPane);
+
+        gridPane.setGridLinesVisible(true);
+
+        // allow button to grow:
+        // button.setMaxWidth(Double.MAX_VALUE);
+        // ask GridPane to make button fill it's cells:
+        // GridPane.setFillWidth(button, true);
+        // button.setMaxWidth(60);
+
+        Label currLabel;
+        for(int i = 0; i < 12; i++) {
+            currLabel = new Label(("Plot " + i));
+            gridPane.add(currLabel, (i/6 + 2), i%6);
+        }
+
+        gridPane.setHgap(20);
+        gridPane.setVgap(20);
+
+
+
+        // Farm plots
+
+
     }
 
 }
