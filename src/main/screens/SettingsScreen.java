eb76723deaa2b7a8ec1f2e1cb9ab141bd3f0be84
HarrisonWut <55332974+HarrisonWut@users.noreply.github.com>
2020-09-24 18:27:19 -0400

    Changes to settings screen listed in status requirements

--- src/main/screens/SettingsScreen.java
+++ src/main/screens/SettingsScreen.java
@@ -27,7 +27,8 @@ public class SettingsScreen extends Screen<VBox> {
 
         root.setPadding(new Insets(10, 50, 50, 50));
         root.setId("settings");
-        root.getStylesheets().addAll(this.getClass().getResource("../css/style.css").toExternalForm());
+        root.getStylesheets().addAll(this.getClass().getResource("../css/style.css")
+                .toExternalForm());
 
         //TITLE
         Label title = new Label("Fun Farm");
@@ -42,86 +43,162 @@ public class SettingsScreen extends Screen<VBox> {
         title.setEffect(ds);
 
         //Position
-        VBox.setMargin(title, new Insets(0,0,0,0));
+        VBox.setMargin(title, new Insets(0, 0, 0, 0));
         title.setPrefWidth(Double.MAX_VALUE);
         title.setAlignment(Pos.TOP_LEFT);
         root.getChildren().add(title);
 
         //NAME
-        Label lName = new Label("Name:");
-        lName.setFont(Font.font(null, FontWeight.BOLD, 14));
-        TextField tfName = new TextField ();
+        Label labelName = new Label("Name:");
+        labelName.setFont(Font.font(null, FontWeight.BOLD, 14));
+        TextField textfieldName = new TextField();
         HBox hb = new HBox();
         root.getChildren().add(hb);
-        hb.getChildren().addAll(lName, tfName);
+        hb.getChildren().addAll(labelName, textfieldName);
         hb.setSpacing(10);
 
         //DIFFICULTY
-        Label lDifficulty = new Label("Choose Difficulty:");
-        lDifficulty.setFont(Font.font(null, FontWeight.BOLD, 14));
-        ToggleGroup tDifficulty = new ToggleGroup();
-        RadioButton bNoobDiff = new RadioButton("Easy");
-        bNoobDiff.setToggleGroup(tDifficulty);
-        RadioButton bNormalDiff = new RadioButton("Medium");
-        bNormalDiff.setToggleGroup(tDifficulty);
-        RadioButton bNSaneDiff = new RadioButton("Hard");
-        bNSaneDiff.setToggleGroup(tDifficulty);
-        root.getChildren().addAll(lDifficulty, bNoobDiff, bNormalDiff, bNSaneDiff);
-        VBox.setMargin(lDifficulty, new Insets(50,0,0,0));
+        Label labelDifficulty = new Label("Choose Difficulty:");
+        labelDifficulty.setFont(Font.font(null, FontWeight.BOLD, 14));
+        ToggleGroup toggleDifficulty = new ToggleGroup();
+        RadioButton buttonEasyDiff = new RadioButton("Easy");
+        buttonEasyDiff.setToggleGroup(toggleDifficulty);
+        buttonEasyDiff.setUserData(Difficulty.EASY);
+        RadioButton buttonNormalDiff = new RadioButton("Medium");
+        buttonNormalDiff.setToggleGroup(toggleDifficulty);
+        buttonNormalDiff.setUserData(Difficulty.MEDIUM);
+        RadioButton buttonHardDiff = new RadioButton("Hard");
+        buttonHardDiff.setToggleGroup(toggleDifficulty);
+        buttonHardDiff.setUserData(Difficulty.HARD);
+        root.getChildren().addAll(labelDifficulty, buttonEasyDiff,
+                buttonNormalDiff, buttonHardDiff);
+        VBox.setMargin(labelDifficulty, new Insets(50, 0, 0, 0));
 
         //SEED
-        Label lSeed = new Label("Choose Seed:");
-        lSeed.setFont(Font.font(null, FontWeight.BOLD, 14));
-        ToggleGroup tSeed = new ToggleGroup();
-        RadioButton bSeed1 = new RadioButton("Carrots");
-        bSeed1.setToggleGroup(tSeed);
-        RadioButton bSeed2 = new RadioButton("Potatoes");
-        bSeed2.setToggleGroup(tSeed);
-        RadioButton bSeed3 = new RadioButton("Celery");
-        bSeed3.setToggleGroup(tSeed);
-        root.getChildren().addAll(lSeed, bSeed1, bSeed2, bSeed3);
-        VBox.setMargin(lSeed, new Insets(50,0,0,0));
+        Label labelSeed = new Label("Choose Seed:");
+        labelSeed.setFont(Font.font(null, FontWeight.BOLD, 14));
+        ToggleGroup toggleSeed = new ToggleGroup();
+        RadioButton buttonSeed1 = new RadioButton("Carrots");
+        buttonSeed1.setToggleGroup(toggleSeed);
+        buttonSeed1.setUserData(Seed.CARROTS);
+        RadioButton buttonSeed2 = new RadioButton("Potatoes");
+        buttonSeed2.setToggleGroup(toggleSeed);
+        buttonSeed2.setUserData(Seed.POTATOES);
+        RadioButton buttonSeed3 = new RadioButton("Celery");
+        buttonSeed3.setToggleGroup(toggleSeed);
+        buttonSeed3.setUserData(Seed.CELERY);
+        root.getChildren().addAll(labelSeed, buttonSeed1, buttonSeed2, buttonSeed3);
+        VBox.setMargin(labelSeed, new Insets(50, 0, 0, 0));
 
         //SEASON
         Label lSeason = new Label("Choose Season:");
         lSeason.setFont(Font.font(null, FontWeight.BOLD, 14));
-        ToggleGroup tSeason = new ToggleGroup();
-        RadioButton bSeason1 = new RadioButton("Spring");
-        bSeason1.setToggleGroup(tSeason);
-        RadioButton bSeason2 = new RadioButton("Summer");
-        bSeason2.setToggleGroup(tSeason);
-        RadioButton bSeason3 = new RadioButton("Fall");
-        bSeason3.setToggleGroup(tSeason);
-        RadioButton bSeason4 = new RadioButton("Winter");
-        bSeason4.setToggleGroup(tSeason);
-        root.getChildren().addAll(lSeason, bSeason1, bSeason2, bSeason3, bSeason4);
-        VBox.setMargin(lSeason, new Insets(50,0,0,0));
+        ToggleGroup toggleSeason = new ToggleGroup();
+        RadioButton buttonSeason1 = new RadioButton("Spring");
+        buttonSeason1.setToggleGroup(toggleSeason);
+        buttonSeason1.setUserData(Season.SPRING);
+        RadioButton buttonSeason2 = new RadioButton("Summer");
+        buttonSeason2.setToggleGroup(toggleSeason);
+        buttonSeason2.setUserData(Season.SUMMER);
+        RadioButton buttonSeason3 = new RadioButton("Fall");
+        buttonSeason3.setToggleGroup(toggleSeason);
+        buttonSeason3.setUserData(Season.FALL);
+        RadioButton buttonSeason4 = new RadioButton("Winter");
+        buttonSeason4.setToggleGroup(toggleSeason);
+        buttonSeason4.setUserData(Season.WINTER);
+        root.getChildren().addAll(lSeason, buttonSeason1,
+                buttonSeason2, buttonSeason3, buttonSeason4);
+        VBox.setMargin(lSeason, new Insets(50, 0, 0, 0));
 
         //CONTINUE
         Button start = new Button("Start Game");
         start.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event) { // TODO @Harrison please also check for unselected options
-                if (tfName.getText().trim().equals("")) {  // empty, blank (?), or null
+            public void handle(ActionEvent event) {
+                if (textfieldName.getText().trim().equals("")) {  // empty, blank (?), or null
                     System.out.println("Handle invalid name");
                     EventQueue.invokeLater(new Runnable() {
                         @Override
                         public void run() {
-                            JOptionPane.showMessageDialog(null, "alert", "alert", JOptionPane.ERROR_MESSAGE);
+                            JOptionPane.showMessageDialog(null, "Please enter a valid name.",
+                                    "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    });
+                } else if (toggleDifficulty.getSelectedToggle() == null) {
+                    System.out.println("Handle unselected difficulty");
+                    EventQueue.invokeLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            JOptionPane.showMessageDialog(null, "Please select a difficulty.",
+                                    "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    });
+                } else if (toggleSeed.getSelectedToggle() == null) {
+                    System.out.println("Handle unselected seed");
+                    EventQueue.invokeLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            JOptionPane.showMessageDialog(null, "Please select a seed.",
+                                    "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    });
+                } else if (toggleSeason.getSelectedToggle() == null) {
+                    System.out.println("Handle unselected season");
+                    EventQueue.invokeLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            JOptionPane.showMessageDialog(null, "Please select a season.",
+                                    "Error", JOptionPane.ERROR_MESSAGE);
                         }
                     });
                 } else {
+                    buttonNormalDiff.setUserData(Difficulty.MEDIUM);
                     manager.state.settings.initialized = true;
-                    manager.state.settings.name = tfName.getText();
-                    manager.state.settings.difficulty = 0; // TODO @Harrison please set the setting appropriatley using enums
-                    manager.state.settings.season = 0; // TODO @Harrison
-                    manager.state.settings.seed = 0; // TODO @Harrison
+                    manager.state.settings.name = textfieldName.getText();
+                    manager.state.settings.difficulty =  ((Difficulty) toggleDifficulty.
+                            getSelectedToggle().getUserData()).getValue();
+                    manager.state.settings.season = ((Season) toggleSeason.
+                            getSelectedToggle().getUserData()).getValue();
+                    manager.state.settings.seed = ((Seed) toggleSeed.
+                            getSelectedToggle().getUserData()).getValue();
                     manager.startGame();
                 }
             }
         });
         root.getChildren().add(start);
-        VBox.setMargin(start, new Insets(50,0,0,0));
+        VBox.setMargin(start, new Insets(50, 0, 0, 0));
     }
+
+    public enum Difficulty {
+        EASY(1), MEDIUM(2), HARD(3);
+        private int value;
+        private Difficulty(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    };
+    public enum Season {
+        SPRING(1), SUMMER(2), FALL(3), WINTER(4);
+        private int value;
+        private Season(int value) {
+            this.value = value;
+        }
+        public int getValue() {
+            return value;
+        }
+    };
+    public enum Seed {
+        CARROTS(1), POTATOES(2), CELERY(3);
+        private int value;
+        private Seed(int value) {
+            this.value = value;
+        }
+        public int getValue() {
+            return value;
+        }
+    };
 
 }
